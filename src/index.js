2ee09be950111a0dd82d7734496363ed2517689b
dzinoviev <danzino21@gmail.com>
2020-11-21 22:40:04 +0200

    Using simple "file-loader"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import Logger from "./logger";
 import Calc from "./calc";
+import src from "./guitar.jpg";
 
 const calc = new Calc();
 const logger = new Logger();
 
 logger.log(calc.sum(1, 2, 3));
+
+const image = document.createElement('img');
+image.src = src;
+document.body.appendChild(image)
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   // mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(svg|png|jpg|jpeg|webp)$/,
+        use: "file-loader",
+      },
+    ],
+  },
 };
