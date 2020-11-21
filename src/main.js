20d4a3c7b2b6b8c912fa16026c418d9c962f40ab
dzinoviev <danzino21@gmail.com>
2020-11-21 13:26:39 +0200

    NPM init & first steps

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/**
+ * This is my App :)
+ */
+class App {
+  run() {
+    const name = "React custom app";
+    console.log(`App "${name}" has been successfully started.`);
+  }
+}
+
+const app = new App();
+app.run();
