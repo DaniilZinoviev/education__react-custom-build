3d934cc014a7b09334cf24ab364bf136c6b82841
dzinoviev <danzino21@gmail.com>
2020-11-21 19:58:11 +0200

    End play with babel

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
-/**
- * This is my App :)
- */
-class App {
-  run = async (name = "React custom app") => {
-    console.log(`App "${name}" has been successfully started.`);
-  }
-}
+import React from "react";
+import ReactDOM from "react-dom";
 
-const app = new App();
-app.run().then(() => console.log('Done.'));
+const App = <p>Hello world</p>;
+
+ReactDOM.render(App, document.getElementById("root"));
