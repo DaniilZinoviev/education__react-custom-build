2f6bb180f0ab9700721286bebef6bb33de3a28ab
dzinoviev <danzino21@gmail.com>
2020-11-22 22:37:53 +0200

    Add environment mods

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,82 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
-      },
-      {
-        test: /\.s[ac]ss$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "./",
-            },
-          },
-          "css-loader",
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.(svg|png|jpe?g|webp)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: "images",
-              name: "[name]-[sha1:hash:7].[ext]",
-            },
-          },
-        ],
-      },
+module.exports = (env = {}) => {
 
-      // Load fonts
-      {
-        test: /\.(otf|ttf|woff|woff2)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: "fonts",
-              name: "[name].[ext]",
+  const { MODE: mode = 'development' } = env;
+
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
+
+  return {
+    mode: isProd ? 'production' : isDev && 'development',
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+        {
+          test: /\.s[ac]ss$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                publicPath: "./",
+              },
             },
-          },
-        ],
-      },
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(svg|png|jpe?g|webp)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: "images",
+                name: "[name]-[sha1:hash:7].[ext]",
+              },
+            },
+          ],
+        },
+
+        // Load fonts
+        {
+          test: /\.(otf|ttf|woff|woff2)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: "fonts",
+                name: "[name].[ext]",
+              },
+            },
+          ],
+        },
+      ],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Webpack application | App",
+        buildTime: new Date().toISOString(),
+        template: "public/index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "main-[hash:4].css",
+      }),
     ],
-  },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Webpack application | App",
-      buildTime: new Date().toISOString(),
-      template: "public/index.html",
-    }),
-    new MiniCssExtractPlugin(),
-  ],
+    devServer: {
+      open: true,
+    },
+  };
 };
