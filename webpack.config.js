f6f93afd92497b8c0c0d36771b35de929d5763f5
dzinoviev <danzino21@gmail.com>
2020-11-22 18:08:51 +0200

    Add plugin to process html

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   // mode: "production",
   module: {
@@ -43,4 +45,10 @@ module.exports = {
       },
     ],
   },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "public/index.html",
+    }),
+  ],
 };
